b8bb897eba464c6db166cd7fc276d012c1f7d6b1
Jeff Eom <jeffeom@gmail.com>
2016-09-09 00:22:02 -0700

    Added button features to images

---
+++ HireDev/HireDev/CreateAccountViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CreateAccountViewController.swift
+//  HireDev
+//
+//  Created by Jeff Eom on 2016-09-09.
+//  Copyright © 2016 Jeff Eom. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountViewController: UIViewController {
+
+}
