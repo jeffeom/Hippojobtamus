a3f3dd00f5240c76d595362153956cc880a00853
Jeff Eom <jeffeom@gmail.com>
2016-09-08 23:57:22 -0700

    first draft of design is done

---
+++ HireDev/HireDev/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.swift
+//  HireDev
+//
+//  Created by Jeff Eom on 2016-09-08.
+//  Copyright © 2016 Jeff Eom. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        self.navigationController?.setNavigationBarHidden(true, animated: true)
+    }
+
+}
