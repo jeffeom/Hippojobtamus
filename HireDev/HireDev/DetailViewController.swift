47baf3f604b115c4088a38a14bf098ebe1b77a3e
Jeff Eom <jeffeom@gmail.com>
2016-10-07 20:17:59 -0700

    Added hide tabbar + navbar function

--- HireDev/HireDev/DetailViewController.swift
+++ HireDev/HireDev/DetailViewController.swift
@@ -71,11 +71,41 @@ class DetailViewController: UIViewController {
         let tap = UITapGestureRecognizer(target: self, action: #selector(dismissFullscreenImage(sender:)))
         newImageView.addGestureRecognizer(tap)
         self.view.addSubview(newImageView)
+        
+        navigationController?.setNavigationBarHidden(navigationController?.isNavigationBarHidden == false, animated: true)
+        setTabBarVisible(visible: !tabBarIsVisible(), animated: true, completion: {_ in })
     }
     
     func dismissFullscreenImage(sender: UITapGestureRecognizer) {
         
         sender.view?.removeFromSuperview()
+        navigationController?.setNavigationBarHidden(navigationController?.isNavigationBarHidden == false, animated: true)
+        setTabBarVisible(visible: !tabBarIsVisible(), animated: true, completion: {_ in })
+    }
+    
+    // pass a param to describe the state change, an animated flag and a completion block matching UIView animations completion
+    func setTabBarVisible(visible: Bool, animated: Bool, completion:@escaping (Bool)->Void) {
+        
+        // bail if the current state matches the desired state
+        if (tabBarIsVisible() == visible) {
+            return completion(true)
+        }
+        
+        // get a frame calculation ready
+        let height = tabBarController!.tabBar.frame.size.height
+        let offsetY = (visible ? -height : height)
+        
+        // zero duration means no animation
+        let duration = (animated ? 0.3 : 0.0)
+        
+        UIView.animate(withDuration: duration, animations: {
+            let frame = self.tabBarController!.tabBar.frame
+            self.tabBarController!.tabBar.frame = frame.offsetBy(dx: 0, dy: offsetY);
+            }, completion:completion)
+    }
+    
+    func tabBarIsVisible() -> Bool {
+        return tabBarController!.tabBar.frame.origin.y < view.frame.maxY
     }
 }
 
