89d76ead1538e80ce293b3d2cf5733cef518463c
Jeff Eom <jeffeom@gmail.com>
2016-10-21 23:56:44 -0700

    fetching bug fix

--- HireDev/HireDev/MasterViewController.swift
+++ HireDev/HireDev/MasterViewController.swift
@@ -36,6 +36,73 @@ class MasterViewController: UITableViewController {
         indicator.bringSubview(toFront: self.view)
         indicator.startAnimating()
         
+        self.fetchDataFromDB()
+        
+        self.navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        
+        let nav = self.navigationController?.navigationBar
+        let font = UIFont.boldSystemFont(ofSize: 18)
+        nav?.titleTextAttributes = [NSFontAttributeName: font]
+        nav?.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white]
+        nav?.tintColor = UIColor.white
+        
+        viewDidLoad()
+        self.navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Segues
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showDetail" {
+            if let indexPath = self.tableView.indexPathForSelectedRow {
+                let categoryContent = self.categoryContents[(indexPath as NSIndexPath).row]
+                let controller = segue.destination as! DetailViewController
+                controller.detailItem = categoryContent as JobItem
+            }
+        }
+    }
+    
+    // MARK: - Table View
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categoryContents.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! JobTableViewCell
+        
+        if (categoryContents.count != 0) {
+            let categoryContents = self.categoryContents[(indexPath as NSIndexPath).row]
+            cell.titleLabel!.text = categoryContents.title
+            cell.commentsLabel.text = categoryContents.date
+            cell.myImageView.image = self.getImageFromString(string: categoryContents.photo)
+            cell.locationLabel.text = categoryContents.location
+        }
+        
+        return cell
+    }
+    
+    func getImageFromString(string: String) -> UIImage {
+        let data: NSData = NSData.init(base64Encoded: string, options: NSData.Base64DecodingOptions.ignoreUnknownCharacters)!
+        let image: UIImage = UIImage.init(data: data as Data)!
+        
+        return image
+    }
+    
+    func fetchDataFromDB() {
         self.ref.child(contents).observe(.value, with: { (snapshot) in
             var newItems: [JobItem] = []
             
@@ -44,7 +111,7 @@ class MasterViewController: UITableViewController {
                 
                 let jobItem = JobItem(snapshot: item as! FIRDataSnapshot)
                 
-                if let theLocation = UserDefaults.standard.string(forKey: "currentLocation")?.replacingOccurrences(of: " ", with: ""){
+                if let theLocation = UserDefaults.standard.string(forKey: "currentLocation"){
                     self.readableOrigin = theLocation
                 }else{
                     let alert = UIAlertController(title: "Current Location Needed", message: "Please set your current location", preferredStyle: UIAlertControllerStyle.alert)
@@ -62,7 +129,7 @@ class MasterViewController: UITableViewController {
                     alert.show()
                 }
                 
-                let readableDestination: String = jobItem.location.replacingOccurrences(of: " ", with: "")
+                let readableDestination: String = jobItem.location
                 
                 self.checkDistance(origin: self.readableOrigin, destination: readableDestination) { (fetchedData) in
                     DispatchQueue.main.async {
@@ -128,69 +195,6 @@ class MasterViewController: UITableViewController {
             self.indicator.hidesWhenStopped = true
             
         })
-        
-        self.navigationController?.setNavigationBarHidden(false, animated: true)
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(true)
-        
-        let nav = self.navigationController?.navigationBar
-        let font = UIFont.boldSystemFont(ofSize: 18)
-        nav?.titleTextAttributes = [NSFontAttributeName: font]
-        nav?.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white]
-        nav?.tintColor = UIColor.white
-        
-        viewDidLoad()
-        self.navigationController?.setNavigationBarHidden(false, animated: true)
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    // MARK: - Segues
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "showDetail" {
-            if let indexPath = self.tableView.indexPathForSelectedRow {
-                let categoryContent = self.categoryContents[(indexPath as NSIndexPath).row]
-                let controller = segue.destination as! DetailViewController
-                controller.detailItem = categoryContent as JobItem
-            }
-        }
-    }
-    
-    // MARK: - Table View
-    
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return categoryContents.count
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! JobTableViewCell
-        
-        if (categoryContents.count != 0) {
-            let categoryContents = self.categoryContents[(indexPath as NSIndexPath).row]
-            cell.titleLabel!.text = categoryContents.title
-            cell.commentsLabel.text = categoryContents.date
-            cell.myImageView.image = self.getImageFromString(string: categoryContents.photo)
-            cell.locationLabel.text = categoryContents.location
-        }
-        
-        return cell
-    }
-    
-    func getImageFromString(string: String) -> UIImage {
-        let data: NSData = NSData.init(base64Encoded: string, options: NSData.Base64DecodingOptions.ignoreUnknownCharacters)!
-        let image: UIImage = UIImage.init(data: data as Data)!
-        
-        return image
     }
 }
 
