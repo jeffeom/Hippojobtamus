bb51886c9007ee5c8bbf4162b0a836b393352279
Jeff Eom <jeffeom@gmail.com>
2016-10-24 20:16:29 -0700

    indicator edit

---
+++ HireDev/HireDev/UIColor + HexColor.swift
@@ -0,0 +1,33 @@
+//
+//  UIColor + HexColor.swift
+//  Hippo
+//
+//  Created by Jeff Eom on 2016-10-24.
+//  Copyright © 2016 Jeff Eom. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController{
+    func hexStringToUIColor (hex:String, alpha:Float) -> UIColor {
+        var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        
+        if (cString.hasPrefix("#")) {
+            cString.remove(at: cString.startIndex)
+        }
+        
+        if ((cString.characters.count) != 6) {
+            return UIColor.gray
+        }
+        
+        var rgbValue:UInt32 = 0
+        Scanner(string: cString).scanHexInt32(&rgbValue)
+        
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(alpha)
+        )
+    }
+}
