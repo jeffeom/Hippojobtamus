ee7764e0fb11a111ab9c9545af3d5a78fa63e194
Jeff Eom <jeffeom@gmail.com>
2017-06-16 21:52:23 -0700

    updated podfile and added in prepareforreuse

--- HireDev/HireDev/JobTableViewCell.swift
+++ HireDev/HireDev/JobTableViewCell.swift
@@ -7,27 +7,32 @@
 //
 
 import UIKit
+import SDWebImage
 
 class JobTableViewCell: UITableViewCell {
+  
+  //MARK: IBOutlets
+  
+  @IBOutlet weak var myImageView: UIImageView!
+  @IBOutlet weak var titleLabel: UILabel!
+  @IBOutlet weak var locationLabel: UILabel!
+  @IBOutlet weak var commentsLabel: UILabel!
+  
+  //MARK: UITableViewCell
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    // Initialization code
+  }
+  
+  override func setSelected(_ selected: Bool, animated: Bool) {
+    super.setSelected(selected, animated: animated)
     
-    //MARK: IBOutlets
-    
-    @IBOutlet weak var myImageView: UIImageView!
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var locationLabel: UILabel!
-    @IBOutlet weak var commentsLabel: UILabel!
-    
-    //MARK: UITableViewCell
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+    // Configure the view for the selected state
+  }
+  
+  override func prepareForReuse() {
+    super.prepareForReuse()
+    self.myImageView.sd_cancelCurrentImageLoad()
+  }
 }
